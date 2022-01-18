fa3541553eca21dffd5cce1a09097250d35cd260
w3schools-test <test@w3schools.com>
2022-01-18 15:31:58 -0600

    added comment to trigger commit In Jenkins added / after src

--- src/main/java/net/yorksolutions/Main.java
+++ src/main/java/net/yorksolutions/Main.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
 //import java.util.List;
 
 //comment to test Github push
-
+//comment to test Github push
 public class Main {
     //create main method
     public static void main(String[] args) {
